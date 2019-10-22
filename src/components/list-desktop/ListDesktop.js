7cbe163abad431e11fc4d0905f8e00e6515b9d2d
Gildas Niyigena <gildniy05@gmail.com>
2019-10-23 00:11:36 +0200

    feature(details)Create selected weather details [Start #169278421]

--- src/components/list-desktop/ListDesktop.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function ListDesktop(props) {
-    return (
-        <div></div>
-    );
-}
-
-export default ListDesktop;
--- src/components/list-mobile/ListMobile.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function ListMobile(props) {
-    return (
-        <div></div>
-    );
-}
-
-export default ListMobile;
