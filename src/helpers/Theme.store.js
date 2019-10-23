4ff791c93ceaa2aa36f72b18a3f8c4c5a385124e
Bonvic Bundi <nyabuyabonvic@gmail.com>
2019-10-23 13:55:42 +0200

    feature(background): Builds a background image - add gif images for the weather - create an object for the images - fetch current weather from app - add inline css for the background [delivers #169321921]

---
+++ src/helpers/Theme.store.js
@@ -0,0 +1,28 @@
+import {
+    clouds,
+    clear,
+    rain,
+    snow,
+    thunder,
+    tornado,
+    fog,
+    drizzle,
+    dust
+} from '../assets/images/'
+
+const theme = {
+    Snow: snow,
+    Thunderstorm: thunder,
+    Drizzle: drizzle,
+    Rain: rain,
+    Clear: clear,
+    Clouds: clouds,
+    Mist: fog,
+    Smoke: fog,
+    Haze: fog,
+    Dust: dust,
+    Fog: fog,
+    Tornado: tornado,
+}
+
+export default theme;
